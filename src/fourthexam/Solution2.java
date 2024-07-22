d9062e59c9a6c770d7eb9308e8fd2ad28a6a8a0b
lseongmi <w2307@e-mirim.hs.kr>
2024-07-22 13:21:41 +0900

    7번 문제 풀이

---
+++ src/fourthexam/Solution2.java
@@ -0,0 +1,4 @@
+package fourthexam;
+
+public class Solution2 {
+}
--- src/thirdexam/Solution.java
+++ src/thirdexam/Solution.java
@@ -8,13 +8,20 @@ class Solution {
     public int[] solution(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
-        while(left < right){
+        for(int i = 0; i < right; i++) {
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
             left += 1;
             right -= 1;
         }
+//        while(left < right){
+//            int temp = arr[left];
+//            arr[left] = arr[right];
+//            arr[right] = temp;
+//            left += 1;
+//            right -= 1;
+//        }
         return arr;
     }
 
