6cb8b31676b77b8cdfa2ce223fc74fa420be9767
lseongmi <w2307@e-mirim.hs.kr>
2024-07-22 10:37:31 +0900

    3번째 문제 풀이

--- src/secondexam/Solution.java
+++ src/secondexam/Solution.java
@@ -7,9 +7,9 @@ class Solution {
         int monthList[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int total = 0;
         for(int i = 0; i < month -1 ; i++)
-            total += i;
-        total += 1;
-        return total - 1;
+            total += monthList[i];
+        total += day;
+        return total -1;
     }
     public int solution(int startMonth, int startDay, 
     		            int endMonth, int endDay) {
