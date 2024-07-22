173521df4f70fe599be381a72298f66338791c4d
lseongmi <w2307@e-mirim.hs.kr>
2024-07-22 10:56:31 +0900

    4번째 문제 풀이

---
+++ src/secondexam/Solution2.java
@@ -0,0 +1,46 @@
+package secondexam;
+
+public class Solution2 {
+    int[] func_a(int[] arr){
+        int[] counter = new int[1001];
+        for(int i = 0; i < arr.length; i++)
+            counter[arr[i]]++;
+        return counter;
+    }
+
+    int func_b(int[] arr){
+        int ret = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(ret < arr[i])
+                ret = arr[i];
+        }
+        return ret;
+    }
+
+    int func_c(int[] arr){
+        final int INF = 1001;
+        int ret = INF;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0 && ret > arr[i])
+                ret = arr[i];
+        }
+        return ret;
+    }
+
+    public int solution(int[] arr) {
+        int[] counter = func_a(arr);
+        int maxCnt = func_b(counter);
+        int minCnt = func_c(counter);
+        return maxCnt / minCnt;
+    }
+
+    // 테스트 케이스 출력을 위한 main 메서드
+    public static void main(String[] args) {
+        Solution2 sol = new Solution2();
+        int[] arr = {1, 2, 3, 3, 1, 3, 3, 2, 3, 2};
+        int ret = sol.solution(arr);
+
+        // 출력 결과 확인
+        System.out.println("Solution: return value of the method is " + ret + " .");
+    }
+}
