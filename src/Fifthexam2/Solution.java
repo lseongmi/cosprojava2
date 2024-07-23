ed19ee5ffaf1e5d6883968f5a75a98ca6b63a108
lseongmi <w2307@e-mirim.hs.kr>
2024-07-23 10:22:36 +0900

    9번 문제 풀이

--- src/Fifthexam2/Solution.java
+++ src/Fifthexam2/Solution.java
@@ -9,7 +9,7 @@ class Solution {
             counter[votes[i]] += 1;
         int answer = -1;
         for(int i = 0; i <= N; ++i)
-            if(counter[i] == K)
+            if(counter[i] >= K)
                 answer += 1;
         return answer;
     }
